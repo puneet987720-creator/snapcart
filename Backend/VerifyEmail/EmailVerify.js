b3c1dad673e565dfc53f7b37660a6b5a868a4bc4
Puneet <puneet987720@gmail.com>
2026-05-30 21:36:36 +0530

    used brevo for email verification

--- Backend/VerifyEmail/EmailVerify.js
+++ Backend/VerifyEmail/EmailVerify.js
@@ -1,40 +1,31 @@
-const nodemailer = require('nodemailer')
+const {BrevoClient} = require('@getbrevo/brevo')
 const dotenv = require('dotenv')
 dotenv.config()
 
 async function verifyEmail(email, token) {
-  if (!process.env.MAIL_USER ) {
-    throw new Error('MAIL_USER is not set')
+  if (!process.env.BREVO_API_KEY ) {
+    throw new Error('BREVO_API_KEY is not set')
   }
-
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      type: 'OAuth2',
-      user: process.env.MAIL_USER,
-      clientId: process.env.OAUTH_CLIENT_ID,
-      clientSecret: process.env.OAUTH_CLIENT_SECRET,
-      refreshToken: process.env.OAUTH_REFRESH_TOKEN,
-    },
+  const brevo = new BrevoClient({
+    apiKey: process.env.BREVO_API_KEY,
   })
 
-  await transporter.verify()
+  try{
+    const result = await brevo.transactionalEmails.sendTransacEmail({
+      subject: 'Verify Your Email',
+      textContent: `click on the link to verify your email: ${process.env.CLIENT_URL}/verify-email/${token}`,
 
-  const sendTestEmail = async () => {
-    try {
-      const info = await transporter.sendMail({
-        from: process.env.MAIL_USER,
-        to: email,
-        subject: 'Verify Your Email',    
-    text: `Please click this link to verify your email: ${process.env.CLIENT_URL}/verify-email/${token}`,
-      })
-      console.log('Verification email sent: %s', info.messageId)
-    } catch (error) {
-      console.error('Error sending verification email:', error)
-      throw error
-    }
+      sender:{
+        name: 'Snapcart Team',
+        email: process.env.MAIL_USER
+      },
+      to: [{ email: email }]
+    })
+    console.log('Verification email sent: %s', result.messageId)
+  }catch(error){
+    console.error('Error sending verification email:', error)
+    throw error
   }
-  await sendTestEmail()
 }
 
 exports.verifyEmail = verifyEmail
